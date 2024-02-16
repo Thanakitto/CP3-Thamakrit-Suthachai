17c3fe01b4f2080018758fc78311819491d9a08a
Thanakitto <159223367+Thanakitto@users.noreply.github.com>
2024-02-16 14:29:08 +0700

    Add files via upload

--- Exercise4_Thanakrit_S.py
+++ Exercise4_Thanakrit_S.py
@@ -1,1 +1,14 @@
+Name = "Thanakrit"
+Surname = "Suthachai"
+print("Name :",Name)
+print("Surname :",Surname)
+print("--- Your Score ---")
+FB = 68
+GB = 80
+IC = 95
+CP = 75
+print("Foundation English :",FB)
+print("General Business :", GB)
+print("Introduction to Computer Systems :", IC)
+print("Computer Programming :", CP)
 
